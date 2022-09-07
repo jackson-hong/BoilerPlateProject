28e986b090961f5537c18d012fdeff1ada5862a9
patrickYoo <yhy1045@naver.com>
2022-09-08 00:17:21 +0900

    jar -> war 변경

--- src/main/java/com/jp/boiler/base/BaseApplication.java
+++ src/main/java/com/jp/boiler/base/BaseApplication.java
@@ -2,12 +2,19 @@ package com.jp.boiler.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BaseApplication {
+public class BaseApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BaseApplication.class);
     }
 
 }
